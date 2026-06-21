17e171cbdae4b4d980072614fd2eab764a82b461
elise <135685125+elisedemarie@users.noreply.github.com>
2026-06-21 12:33:37 +1000

    Add end marker to trie and tests.

--- src/trie.rs
+++ src/trie.rs
@@ -5,22 +5,28 @@ type NodeId = usize;
 #[derive(Debug, Clone)]
 struct TrieNode {
     children: BTreeMap<char, NodeId>,
+    pub is_end: bool,
 }
 
 impl TrieNode {
     pub fn new() -> Self {
         Self {
             children: BTreeMap::new(),
+            is_end: false
         }
     }
 
     pub fn add_child(&mut self, character: char, child_id: NodeId) {
         self.children.insert(character, child_id);
     }
+
+    pub fn set_as_end(&mut self) {
+        self.is_end = true 
+    }
 }
 
 #[derive(Debug, Clone)]
-struct Trie {
+pub struct Trie {
     trie_nodes: Vec<TrieNode>,
 }
 
@@ -41,7 +47,7 @@ impl Trie {
         self.trie_nodes.get(id)
     }
 
-    pub fn add_keyword(&mut self, keyword: &String) {
+    fn add_keyword(&mut self, keyword: &str) {
         let mut node_id: NodeId = 0;
         for character in keyword.chars() {
             let node = self.get_mut_node(node_id).expect("There should be a node here.");
@@ -53,6 +59,23 @@ impl Trie {
                 }
             }
         } 
+        self.get_mut_node(node_id).expect("There should be a node here.").set_as_end();
+    }
+
+    fn follow(&self, path: &str) -> Option<NodeId> {
+        let mut id = 0;
+        for character in path.chars() {
+            id = *self.get_node(id)?.children.get(&character)?;
+        }
+        Some(id)
+    }
+
+    pub fn build_trie(keywords: &[&str]) -> Self {
+        let mut trie = Self::default();
+        for word in keywords {
+            trie.add_keyword(word);
+        }
+        trie
     }
 }
 
@@ -87,32 +110,100 @@ use super::*;
 
     #[test]
     fn add_keyword_builds_trie() {
-        let keyword = "abcd".to_string(); 
-        let mut trie = Trie::default();
-        trie.add_keyword(&keyword);
+        let k1 = "abcd";
+        let keywords = [k1];
+        let trie = Trie::build_trie(&keywords);
         let res = traverse_trie_in_bfs(trie.clone());
         assert_eq!(res, "abcd");
     }
 
     #[test]
     fn add_2_keyword_builds_trie() {
-        let k1 = "abcd".to_string(); 
-        let k2 = "efgh".to_string();
-        let mut trie = Trie::default();
-        trie.add_keyword(&k1);
-        trie.add_keyword(&k2);
+        let k1 = "abcd"; 
+        let k2 = "efgh";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
         let res = traverse_trie_in_bfs(trie.clone());
         assert_eq!(res, "aebfcgdh");
     }
 
     #[test]
     fn add_2_keyword_with_same_first_character_builds_trie() {
-        let k1 = "abcd".to_string(); 
-        let k2 = "afgh".to_string();
-        let mut trie = Trie::default();
-        trie.add_keyword(&k1);
-        trie.add_keyword(&k2);
+        let k1 = "abcd"; 
+        let k2 = "afgh";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
         let res = traverse_trie_in_bfs(trie.clone());
         assert_eq!(res, "abfcgdh");
+    }
+
+    #[test]
+    fn trie_shares_common_prefix_between_keywords() {
+        let k1 = "abcd";
+        let k2 = "afgh";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
+        let id = trie.follow("a").expect("a should exist.");
+        let node = trie.get_node(id).unwrap();
+        assert_eq!(node.children.len(), 2);
+    }
+
+    #[test]
+    fn trie_does_not_duplicate_existing_path() {
+        let k1 = "abcd";
+        let k2 = "afgh";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
+        let id = trie.follow("a").expect("a should exist.");
+        let node = trie.get_node(id).unwrap();
+        assert_eq!(node.children.len(), 2);
+    }
+
+    #[test]
+    fn final_letter_is_end_true () {
+        let k1 = "abcd";
+        let keywords = [k1];
+        let trie = Trie::build_trie(&keywords);
+        let id = trie.follow(k1).expect("path should exist.");
+        let node = trie.get_node(id).unwrap();
+        assert!(node.is_end);
+    }
+
+    #[test]
+    fn mid_trie_keyword_end_is_end() {
+        let k1 = "abcd";
+        let k2 = "ab";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
+        let id = trie.follow(k2).expect("path should exist.");
+        let node = trie.get_node(id).unwrap();
+        assert!(node.is_end);
+    }
+
+    #[test]
+    fn only_end_words_is_end() {
+        let k1 = "abcd";
+        let k2 = "ab";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
+        let non_end_paths = ["a", "abc"];
+        for path in non_end_paths {
+            let id = trie.follow(path).expect("path should exist.");
+            let node = trie.get_node(id).unwrap();
+            assert!(!node.is_end);
+        }
+    }
+
+    #[test]
+    fn end_on_multiple_branches_is_end() {
+        let k1 = "abcd";
+        let k2 = "efgh";
+        let keywords = [k1, k2];
+        let trie = Trie::build_trie(&keywords);
+        for path in keywords {
+            let id = trie.follow(path).expect("path should exist.");
+            let node = trie.get_node(id).unwrap();
+            assert!(node.is_end);
+        }
     }
 }
